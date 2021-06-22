5291d80e60667909344b186c6540d05e13bc510f
Mario Salgado <mapsalgado92@gmail.com>
2021-06-22 17:57:53 +0100

    Fixed convertTime from snippets

--- snippets/date-handling.js
+++ snippets/date-handling.js
@@ -28,9 +28,11 @@ export const convertTime = (time) => {
   let out = 0
   if (timeAux[0][0] === 12) {
     timeAux[1] === "AM" ? out = "00" + timeAux[0][1] : out = "12" + timeAux[0][1]
+  } else {
+    timeAux[1] === "AM" ? out = timeAux[0][0] * 100 + timeAux[0][1] : out = timeAux[0][0] * 100 + timeAux[0][1] + 1200
+    out = String(out)
   }
-  timeAux[1] === "AM" ? out = timeAux[0][0] * 100 + timeAux[0][1] : out = timeAux[0][0] * 100 + timeAux[0][1] + 1200
-  out = String(out)
+
   if (out.length === 2) {
     out = "0" + out
   }
