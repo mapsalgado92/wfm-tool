3e38958b2f6dc50d5b47b71b2d547f76791cd9b4
Mario Salgado <mapsalgado92@gmail.com>
2021-07-16 01:34:54 +0100

    added intraday converter

--- components/converters/IntradayConverter.js
+++ components/converters/IntradayConverter.js
@@ -15,21 +15,25 @@ const IntradayConverter = ({ raw, exportConverted }) => {
 
     let dataRows = raw
 
-    _DATE_TIME = 2;
-    _CONTACTS = 3;
-    _AHT = 9;
-    _SL = 11;
-    _REQUIRED = 21;
-    _ACT_CONTACTS = 4
-    _ACT_AHT = 10
-    _ACT_SL = 13
-    _ACT_REQUIRED = 23
+    let _DATE_TIME = 1;
+    let _CONTACTS = 2;
+    let _AHT = 8;
+    let _SL = 10;
+    let _REQUIRED = 20;
+    let _ACT_CONTACTS = 3
+    let _ACT_AHT = 9
+    let _ACT_SL = 12
+    let _ACT_REQUIRED = 22
 
     let header = ["DATE", "TIME", "VOLUMES", "ACT_VOLUMES", "AHT", "ACT_AHT", "SL", "ACT_SL", "REQ", "ACT_REQ"]
     let entries = []
     let dates = []
 
-    for (i = 9; i < dataRows.length - 15; i++) {
+    let current = {
+      date: null
+    }
+
+    for (let i = 9; i < dataRows.length - 15; i++) {
       let aux = dataRows[i][_DATE_TIME]
       if (/Date: /.test(aux)) {
         current.date = dateToString(stringToDate(aux.split(":")[1]))
@@ -37,10 +41,12 @@ const IntradayConverter = ({ raw, exportConverted }) => {
           dates.push(current.date)
         }
       } else if (/[0-9]+:[0-9]+/.test(aux) || /Total/.test(aux) || /Average/.test(aux)) {
-        let newEntry = [current.date, aux, dataRows[i][_CONTACTS], dataRows[i][_AHT], dataRows[i][_SL], dataRows[i][_REQUIRED], dataRows[i][_OPEN]]
+        let newEntry = [current.date, aux, dataRows[i][_CONTACTS], dataRows[i][_ACT_CONTACTS], dataRows[i][_AHT], dataRows[i][_ACT_AHT], dataRows[i][_SL], dataRows[i][_ACT_SL], dataRows[i][_REQUIRED], dataRows[i][_ACT_REQUIRED]]
         entries.push(newEntry)
       }
     }
+
+    console.log(entries)
 
     exportConverted({ header, entries })
     setEntries({ data: [header, ...entries], type: "intraday", dates })
